dfaec25b2776caad9a5bb791822343edec6e10b8
Meatperson <76818683+Meatperson@users.noreply.github.com>
2021-01-06 20:23:35 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mixed Messages
+
+## Introduction
+
+Message generator which outputs random jokes everytime the user runs the program.
+
+## Technologies
+
+* Javascript ES9
+* Other languages maybe?
---
+++ script.js
@@ -0,0 +1,7 @@
+let components = {
+    beggining: ["What do you get", "How can", "What does", "Why did"],
+    subject: ["a person", "the pope", "a dog", "Chuck Norris", "Oprah", "a bucket of milk", "you"],
+    middle: ["for his birthday?", "get as hard as a rock?", "on your birthday?", "on top of birthday cakes?"],
+    object: ["a candle", "jet skis", "popsicles", "a parachute", "fantastic dance moves", "two litters of any liquid you can think of"],
+    ending: ["in the cake", "feeling crumbry", "to roll with", "and a happy ending"]
+};
