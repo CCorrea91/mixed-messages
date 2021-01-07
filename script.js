89a01c0f040326258047bacad4f90e2218764010
Meatperson <76818683+Meatperson@users.noreply.github.com>
2021-01-07 19:47:23 +0000

    Added some more phrases, jokes still make no sense

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let parts = [
     beggining = ["What do you get when", "How can", "What does", "Why did"],
     subject = ["a person", "the pope", "a dog", "Chuck Norris", "Oprah", "a bucket of milk", "you"],
-    verbs = ["get", "do", "have", "eat", "pump"],
+    verbs = ["get", "do", "have", "eat", "pump", "steal", "kick", "watch"],
     middle = ["at his birthday?", "as hard as a rock?", "on your birthday?", "on top of birthday cakes?"],
     object = ["a candle", "jet skis", "popsicles", "a parachute", "fantastic dance moves", "two litters of any liquid you can think of"],
-    ending = ["in the cake", "feeling crumbry", "to roll with", "and a happy ending"]
+    ending = ["in the face", "feeling crumbry", "to roll with", "and a happy ending", "and why wouldn't you"]
 ];
-
 
 let randomizer = array => Math.floor(Math.random() * array.length);
 
@@ -20,5 +19,3 @@ let builder = array => {
 };
 
 builder(parts);
-
-
